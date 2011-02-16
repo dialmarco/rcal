5fe8c06367e26e8b5e9bb2da2665cb344877019c
Diego Alonso <diego@diegoalonso.net>
2011-02-16 12:30:20 +1100

    making practice entry controller date

--- app/controllers/practice_entries_controller.rb
+++ app/controllers/practice_entries_controller.rb
@@ -9,7 +9,10 @@ class PracticeEntriesController < ApplicationController
     practice = Practice.find(params[:practice])
     user_practice = UserPractice.find_by_user_id_and_practice_id(current_user.id, practice.id)
     amount = params[:amount]
-    PracticeEntry.create!(:user => current_user, :user_practice => user_practice, :amount => amount)
+    param_date = params[:date]  
+    date = Date.new(param_date["year"].to_i, param_date["month"].to_i, param_date["day"].to_i)
+    puts date
+    PracticeEntry.create!(:user => current_user, :user_practice => user_practice, :amount => amount, :practice_date => date)
     redirect_to "/"
   end
 end
