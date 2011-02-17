55f719c853594688e609ebd4450ba3dfa51550ae
Diego Alonso <diego@diegoalonso.net>
2011-02-18 08:39:23 +1100

    chart for current month

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -1,8 +1,7 @@
 class JavascriptsController < ApplicationController
   before_filter :authenticate_user!
 
-  # expose(:again) { [[3,9],[4,5],[5,9],[6,7]] }
-  expose(:again) { PracticeEntry.current_month.collect { |it| [it.practice_date.to_time.to_i, it.amount] } }
+  expose(:again) { PracticeEntry.current_month }
 
   def monthly_practice
   end
--- app/models/practice_entry.rb
+++ app/models/practice_entry.rb
@@ -7,6 +7,26 @@ class PracticeEntry < ActiveRecord::Base
   def self.current_month
     today = Date.today
     first_of_the_month = Date.new(today.year, today.month, 1)
-    where(:practice_date >= first_of_the_month)
+    days_in_month = Date.civil(today.year, today.month, -1).day
+    last_of_the_month = Date.new(today.year, today.month, days_in_month)
+
+    practices = where(:practice_date >= first_of_the_month).where(:practice_date <= last_of_the_month)
+
+    as_stats = {}
+    practices.each do |pract|
+      has_first = true if pract.practice_date == first_of_the_month
+      has_last = true if pract.practice_date == last_of_the_month
+      as_stats[pract.practice_date.to_time.to_i * 1000] = pract.amount
+    end
+
+    (first_of_the_month..last_of_the_month).collect do |day|
+      day_as_utc = day.to_time.to_i * 1000
+      if (as_stats[day_as_utc])
+        [day_as_utc, as_stats[day_as_utc]]
+      else
+        [day_as_utc, 0]
+      end
+    end
+
   end
 end
